6fdefc04fd62646d7f84ed91df9c2bf904a95205
Trinh Doan <trinhd997@gmail.com>
2026-01-19 09:34:21 +0200

    first working version

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import {useState} from 'react';
-import reactLogo from '/src/assets/images/icon-success.svg';
+import iconSuccess from '/src/assets/images/icon-success.svg';
 import './App.css';
 
 function App() {
     const [email,setEmail]=useState("");
     const [isConfirmVisible,setisConfirmVisible]=useState(false);
+    const [isError, setIsError]=useState(false);
 
     function handleSubmit(e) {
         e.preventDefault();
-        setisConfirmVisible(true);
+        if(email.trim()) {
+            //TODO: we could do more of email validation here, currently html the email validation by itself?????
+
+            if(isError) {
+                setIsError(false);
+                addRemoveEmailErrorClass(false);
+            }
+
+            setisConfirmVisible(true);
+        }
+        else if(!isError) {
+            setIsError(true);
+            addRemoveEmailErrorClass(true);
+        }
     }
 
     function handleDismiss() {
         setisConfirmVisible(false);
         setEmail("");
+    }
+
+    function addRemoveEmailErrorClass(showError) {
+        const emailElement=document.getElementById("email");
+        if(emailElement.classList.contains("error")) {
+            emailElement.classList.remove("error");
+        }
+        if(showError) {
+            emailElement.classList.add("error");
+        }
     }
     return (
         <>
@@ -31,8 +55,11 @@ function App() {
                                 <li className="list-item">And much more!</li>
                             </ul>
                             <div className="input-cotainer">
-                                <label className="label" id="email-label" htmlFor="email">Email address</label>
-                                <input className="input email" type="email" id="email" value={email} onInput={(event) => setEmail(event.target.value)} />
+                                <div className="label-and-error">
+                                        <label className="label" id="email-label" htmlFor="email">Email address</label>
+                                        {isError && <p className="error-message">A valid email required</p>}
+                                </div>
+                                <input className="email" type="email" id="email" value={email} onInput={(event) => setEmail(event.target.value)} />
                             </div>
                             <button type="submit" className="btn submit-btn">Subsrcibe to monthly newsletter</button>
                         </div>
@@ -42,7 +69,7 @@ function App() {
                     isConfirmVisible &&
                     <div className="confirm-container">
                         <div className="confirm-message">
-                            <img src={reactLogo} className="logo react" alt="React logo" />
+                            <img src={iconSuccess} className="icon-success" alt="icon success" />
                             <h2>Thanks for subscribing!</h2>
                             <p>A confirmation has been sent to your {email}. Please open it and click the button inside to confirm the subscription</p>
                         </div>
